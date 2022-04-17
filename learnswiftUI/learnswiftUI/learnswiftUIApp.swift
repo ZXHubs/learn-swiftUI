92cc0713927a611b97cac0e3abcb1cdc5b7d0a3e
ZhangX <zxjerry@163.com>
2022-04-17 13:28:49 +0800

    Build an iOS app

---
+++ learnswiftUI/learnswiftUI/learnswiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnswiftUIApp.swift
+//  learnswiftUI
+//
+//  Created by ZhangX on 2022/4/17.
+//
+
+import SwiftUI
+
+@main
+struct learnswiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
