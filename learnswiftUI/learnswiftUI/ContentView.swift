1fd715a7ec25e70ed376aafb0c2d546667ceaa32
ZhangX <zxjerry@163.com>
2022-04-18 16:28:01 +0800

    visual editor

--- learnswiftUI/learnswiftUI/ContentView.swift
+++ learnswiftUI/learnswiftUI/ContentView.swift
@@ -9,8 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            VStack(alignment: .leading, spacing: 8.0) {
+                Spacer()
+                Image("Logo 2")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 26.0, height: 26.0)
+                    .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+                Text("swiftUI for iOS 15")
+                    .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                    .fontWeight(.bold)
+                Text("20 section - 3 hours")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
+                    .font(.footnote)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(2)
+            }
+            .padding(/*@START_MENU_TOKEN@*/.all, 20.0/*@END_MENU_TOKEN@*/)
+            .frame(height: 350.0)
+            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("Background")/*@END_MENU_TOKEN@*/)
+            .cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
+            .shadow(radius: /*@START_MENU_TOKEN@*/20/*@END_MENU_TOKEN@*/)
+        }
     }
 }
 
