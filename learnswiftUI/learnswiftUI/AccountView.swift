0f2678f9928a0a96eb620ec9ea3ed4182b693cad
ZhangX <zxjerry@163.com>
2022-04-30 19:36:10 +0800

    Navigation Link and URL

--- learnswiftUI/learnswiftUI/AccountView.swift
+++ learnswiftUI/learnswiftUI/AccountView.swift
@@ -38,12 +38,31 @@ struct AccountView: View {
                 .frame(maxWidth: .infinity)
                 .padding()
                 Section {
-                    Label("Settings", systemImage: "gear")
-                    Label("Billing", systemImage: "creditcard")
-                    Label("Help", systemImage: "questionmark")
+                    NavigationLink(destination: ContentView()) {
+                        Label("Settings", systemImage: "gear")
+                    }
+                    NavigationLink{ Text("Billing")} label: {
+                        Label("Billing", systemImage: "creditcard")
+                    }
+                    NavigationLink{ ContentView()} label:  {
+                        Label("Help", systemImage: "questionmark")
+                    }
                 }
+                .accentColor(.primary)
                 .listRowSeparatorTint(.blue)
                 .listRowSeparator(.hidden)
+                
+                Section {
+                    Link(destination: URL(string: "https://www.apple.com.cn")!) {
+                        HStack {
+                            Label("Website", systemImage: "house")
+                            Spacer()
+                            Image(systemName: "link")
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                }
+                .accentColor(.primary)
             }
             .listStyle(.insetGrouped)
             .navigationTitle("Account")
